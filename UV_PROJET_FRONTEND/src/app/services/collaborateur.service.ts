8e349641957bbea7581b3b2345d8e40bec6b236a
Noobs440 <arieldoubissi330@gmail.com>
2025-06-26 13:53:21 +0200

    projet squelette v1

---
+++ UV_PROJET_FRONTEND/src/app/services/collaborateur.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap, catchError, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CollaborateurService {
+
+  constructor(private http:HttpClient){}
+
+
+  geCollaborateurs(): Observable<any[]>{
+    return this.http.get<any[]>('http://localhost:8000/api/ressources/collaborateurs').pipe(
+      tap((response)=>console.table(response)),
+      catchError((error) =>{
+        console.log(error);
+        return of([]);
+      })
+    )
+  }
+
+  addCollaborateur(nom_collab:string , email_collab:string):Observable<any>{
+    return this.http.post<any>('http://localhost:8000/api/ressources/collaborateurs', {nom_collab , email_collab});
+  }
+
+  deleteCollaborateur(id:string):Observable<any>{
+    return this.http.delete(`http://localhost:8000/api/ressources/collaborateurs/${id}`);
+  }
+
+  updateCollaborateur(id:string ,nom_collab:string , email_collab:string):Observable<any>{
+    return this.http.put<any>(`http://localhost:8000/api/ressources/collaborateurs/${id}`, {nom_collab , email_collab});
+  }
+}
